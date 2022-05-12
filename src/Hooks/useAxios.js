1ba02f39f7fc3434f201d4a3de7125126601213c
Purva Sharma <71826879+purvasharmadev@users.noreply.github.com>
2022-05-12 14:11:06 +0530

    refactor: added params

--- src/Hooks/useAxios.js
+++ src/Hooks/useAxios.js
@@ -1,33 +1,24 @@
 import axios from "axios"
-import {useState,useEffect} from "react"
+import {useState} from "react"
 
-function useAxios(url) {
+function useAxios() {
     const [response,setResponse] = useState([])
     const [loading,setLoading] = useState(true)
     const [error,setError] = useState("")
 
-  async function fetchData(){
+  async function fetchData(params){
       try {
-          let result = await axios.get(url)
-          if(result.status === 200){
+          let result = await axios.request(params)
               setResponse(result.data)
-          }
-          setLoading(false)
       } catch (error) {
-          console.error(error)
-          setError("Something went wrong!, try again later!")
+          console.error(error.response.data.errors[0])
+          setError(error.response.data.errors[0])
+      } finally{
+        setLoading(false)
       }
     }
 
-    useEffect(()=>{
-        setTimeout(() => {
-            fetchData()
-        }, 2000);
-        setLoading(true)
-        // eslint-disable-next-line react-hooks/exhaustive-deps
-    },[url])
-
-  return {response,loading,error}
+  return {response,loading,error,fetchData}
 }
 
 export  {useAxios}
