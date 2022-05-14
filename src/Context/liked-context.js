7edc983a394480371f539cade1c63065835b0e2b
Purva Sharma <71826879+purvasharmadev@users.noreply.github.com>
2022-05-14 22:30:15 +0530

    fix: handle the res status and added toast

--- src/Context/liked-context.js
+++ src/Context/liked-context.js
@@ -1,12 +1,10 @@
-import { createContext,useContext,useEffect,useState } from "react";
-// import {useAxios} from "../Hooks/useAxios"
+import { createContext,useContext,useState } from "react";
 import axios from "axios";
 import { toast } from "react-toastify";
 
 const LikedContext = createContext()
 
 function LikedProvider({children}){
-    // const {response,error,loading,fetchData} = useAxios();
     const [likedVideo, setLikedVideo] = useState([])
 
     const api = "/api/user/likes"
@@ -24,7 +22,6 @@ function LikedProvider({children}){
           }
         );
         if (res.status === 201) {
-            console.log("ress ", res.data.likes)
             setLikedVideo(res.data.likes);
             toast.success("You liked!",{id:"like-success",position: toast.POSITION.TOP_RIGHT,
             autoClose:2000})
@@ -33,7 +30,6 @@ function LikedProvider({children}){
   
       }
      catch (error) {
-      console.log("error is: ", error);
       toast.error(error.response.data.errors[0],{id:"like-error",position: toast.POSITION.TOP_RIGHT,
       autoClose:2000})
     }
@@ -42,50 +38,23 @@ function LikedProvider({children}){
 
   async function removeFromLiked(item) {
     try {
-      const res = await axios.delete(`api/user/likes/${item._id}`, {
+      const res = await axios.delete(`/api/user/likes/${item._id}`, {
         headers: {
           "content-type": "text/json",
           authorization: localStorage.getItem("userToken"),
         },
       });
       if (res.status === 200) {
-        console.log("ress ", res.data.likes)
         setLikedVideo(res.data.likes);
         toast.success("You unliked!",{id:"like-remove-success",position: toast.POSITION.TOP_RIGHT,
         autoClose:2000})
       }
     } catch (error) {
-      console.log("error is :", error);
-      toast.error(error.response.data.errors,{id:"like-remove-error",position: toast.POSITION.TOP_RIGHT,
+      toast.error(error.response.data.errors[0],{id:"like-remove-error",position: toast.POSITION.TOP_RIGHT,
       autoClose:2000})
 
     }
   }
-
-
-
-
-// function addToLiked(item){
-//         fetchData({
-//             method:"post",
-//             url:api,
-//             data:{ video: item },
-//             headers: {
-//                 "content-type": "text/json",
-//                 authorization: localStorage.getItem("userToken"),
-//             },
-//         });
-//         setLikedVideo(response.likes)
-//         console.log("res addToLike ", response.likes)
-// }
-
-// useEffect(()=>{
-//     if(likedVideo.length > 0){
-//         return likedVideo
-//     }
-// },[likedVideo])
-
-console.log("likedVideo ", likedVideo)
 
 
     return(
