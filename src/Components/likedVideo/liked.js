79ef8d85a7a9f4e64d4e8526ed087d4374d309d4
Purva Sharma <71826879+purvasharmadev@users.noreply.github.com>
2022-05-15 09:11:31 +0530

    docs: minor change

--- src/Components/likedVideo/liked.js
+++ src/Components/likedVideo/liked.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { useLiked } from "../../Context/liked-context";
 import { BsFillTrashFill } from "react-icons/bs";
-import { Link, useNavigate } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 
 function LikedVideo() {
     const navigateTo = useNavigate();
