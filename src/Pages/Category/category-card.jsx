d89795a5838c61b60915f9b4727d427935656fde
Purva Sharma <71826879+purvasharmadev@users.noreply.github.com>
2022-05-19 09:45:49 +0530

    feat: added navigateToExplorePage

--- src/Pages/Category/category-card.jsx
+++ src/Pages/Category/category-card.jsx
@@ -1,20 +1,29 @@
-import React from 'react';
+import React from "react";
+import { useNavigate } from "react-router-dom";
+import { useVideo } from "../../Context/video-context";
 
-function CategoryCard(props){
-    return (
-        <>
-        <div className="card card-overlay">
-          <img
-            className="img-responsive w-100"
-            src={props.Img}
-            alt={props.Name}
-          />
-          <div className="card-body">
-            <h3 className="card-text">{props.Name}</h3>
-          </div>
-        </div>        
-        </>
-    )
+function CategoryCard(props) {
+  const navigateTo = useNavigate();
+  const { filterDispatch } = useVideo();
+
+  const navigateToExplorePage = () => {
+    filterDispatch({ type: "Category", payload: props.Name });
+    navigateTo(`/explore/`);
+  };
+  return (
+    <>
+      <div onClick={navigateToExplorePage} className="card card-overlay">
+        <img
+          className="img-responsive w-100"
+          src={props.Img}
+          alt={props.Name}
+        />
+        <div className="card-body">
+          <h3 className="card-text">{props.Name}</h3>
+        </div>
+      </div>
+    </>
+  );
 }
 
-export {CategoryCard};
+export { CategoryCard };
