c29f3f329dd519d4ae304c1ce1a71d42746b043d
Purva Sharma <71826879+purvasharmadev@users.noreply.github.com>
2022-05-19 09:43:45 +0530

    feat: added reducer for filter

--- src/Context/video-context.js
+++ src/Context/video-context.js
@@ -1,27 +1,54 @@
-import { createContext,useContext,useEffect } from "react";
-import {useAxios} from "../Hooks/useAxios"
+import { createContext, useContext, useEffect, useReducer } from "react";
+import { useAxios } from "../Hooks/useAxios";
 
-const VideoContext = createContext()
+const VideoContext = createContext();
 
-function VideoProvider({children}){
-    const {response,error,loading,fetchData} = useAxios()
-    
+function VideoProvider({ children }) {
+  const { response, error, loading, fetchData } = useAxios();
 
-    useEffect(()=>{
-        fetchData({
-         method:"get",
-         url:"/api/videos"
-        })
-// eslint-disable-next-line react-hooks/exhaustive-deps
-    },[response])
-  
-    return(
-        <VideoContext.Provider value={{video:response.videos,error,loading}}>
-            {children}
-        </VideoContext.Provider>
-    )
+  function filterReducer(state, action) {
+    switch (action.type) {
+      case "Category":
+        return {
+          ...state,
+          categoryName: action.payload,
+        };
+
+      default:
+        return state;
+    }
+  }
+  //  Filter Reducer
+  const [filterState, filterDispatch] = useReducer(filterReducer, {
+    search_query: "",
+    categoryName: "",
+  });
+
+  //   console.log("filterState: ", filterState);
+
+  useEffect(() => {
+    fetchData({
+      method: "get",
+      url: "/api/videos",
+    });
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [response]);
+
+  return (
+    <VideoContext.Provider
+      value={{
+        video: response.videos,
+        error,
+        loading,
+        filterDispatch,
+        filterState,
+      }}
+    >
+      {children}
+    </VideoContext.Provider>
+  );
 }
 
-const useVideo = ()=> useContext(VideoContext)
+const useVideo = () => useContext(VideoContext);
 
-export {VideoProvider,useVideo}
+export { VideoProvider, useVideo };
--- src/Pages/Explore/explore-page.jsx
+++ src/Pages/Explore/explore-page.jsx
@@ -1,16 +1,39 @@
 import "./explore-page.css";
+import { useEffect,useState } from "react";
+
 import { useVideo } from "../../Context/video-context";
 import { useWatchLater } from "../../Context/watchlater-context";
 import { BsFillBookmarkPlusFill, BsFillTrashFill } from "react-icons/bs";
 import {useWatchHistory} from "../../Context/watchhistory-context";
 import { useNavigate } from "react-router-dom";
+import {useCategory} from "../../Context/category-context"
+
 
 function Explore() {
+  // const [categories,setCategories] = useState()
+
+  
+
+
   const navigateTo = useNavigate();
-  const { video, loading } = useVideo();
+  const { video, loading,filterState,filterDispatch } = useVideo();
+  // console.log("categoryId ",categoryId)
+  const [allVideos,setAllVideos]= useState(video)
+  const {category} = useCategory()
+  // console.log("cate ", category)
+  console.log("all video ", allVideos)
+  // console.log("state ", filterState)
+  // console.log("video ", video)
+
   const { watchLaterVideo, addTowatchLater, removeFromwatchLater } =
     useWatchLater();
     const {addToWatchHistory} = useWatchHistory()
+
+
+    const applyFilter = ()=>{
+     const filteredVideo = video.filter((item)=>item.category === filterState.categoryName)
+     setAllVideos(filteredVideo)
+    }
 
     function addToWatchHistoryHandler(item){
       addToWatchHistory(item)
@@ -24,18 +47,46 @@ function Explore() {
   function removeFromwatchLaterHandler(item) {
     removeFromwatchLater(item);
   }
+
+  // useEffect(()=>{
+  //   setAllVideos(video)
+
+  // },[])
+
+  useEffect(()=>{
+    console.log("useEffect for applyFilters")
+    if(filterState.categoryName){
+      applyFilter()
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+
+  },[filterState])
+
+
   return (
     <>
       <h2 className="text-center color-primary">Explore</h2>
+      <div className="flex flex-space-between w-50 m-auto flex-wrap">
+      <button style={{backgroundColor : filterState.categoryName === "" ? "var(--PRIMARY-COLOR)" : "", color:filterState.categoryName === "" ? "var(--TEXT-COLOR)" : ""}} onClick={()=>{
+          filterDispatch({type:"Category",payload:""})
+          setAllVideos(video)
+        }} className="category-btn">All</button>
+      {category && category.map((item)=>{
+        return <button style={{backgroundColor:item.categoryName === filterState.categoryName ? "var(--PRIMARY-COLOR)":"",color:item.categoryName === filterState.categoryName ? "var(--TEXT-COLOR)":""}} onClick={()=>{
+          applyFilter()
+          filterDispatch({type:"Category",payload:item.categoryName})
+        }} className="category-btn">{item.categoryName}</button>
+      } )}
+      </div>
+   
       <div className="card-container mb-1">
-        {loading && <h1>loading.....</h1>}
-        {video &&
-          video.map((item) => {
+        {loading &&             <div className="flex flex-space-center align-item-center h-100">loading.....</div>}
+        {allVideos &&
+          allVideos.map((item) => {
             return (
               <div className="video-card">
                 <div
                   onClick={() => {
-                    console.log("clicked!!");
                     navigateTo(`/explore/${item._id}`);
                     addToWatchHistoryHandler(item)
                   }}
