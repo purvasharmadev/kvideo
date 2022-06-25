f67bad8e5c3ce6efc279461c4fedcf328878b597
Purva Sharma <71826879+purvasharmadev@users.noreply.github.com>
2022-06-25 21:28:05 +0530

    style: added page not found

--- src/Pages/PageNotFound/page-not-found.jsx
+++ src/Pages/PageNotFound/page-not-found.jsx
@@ -1,8 +1,12 @@
-import React from 'react'
+import React from 'react';
+import pageNotFound from "../../Assets/Images/pageNotFound.svg";
 
 function PageNotFound() {
   return (
-    <div>PageNotFound</div>
+    <div className='m-auto h-100 w-50 text-center'>
+      <img src={pageNotFound} height="300px" alt="404"/>
+      <h2>Page Not Found!</h2>
+    </div>
   )
 }
 
